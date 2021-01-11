829d2d624e43bb92146e1cb3645390f788d2e37b
Durmix <Durmix@users.noreply.github.com>
2021-01-11 21:57:14 +0100

    Drobne poprawki

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -9,23 +9,23 @@ public class Node {
 		this.value = value;
 	}
 
-	public int getValue() {
+	int getValue() {
 		return value;
 	}
 
-	public Node getPrev() {
+	Node getPrev() {
 		return prev;
 	}
 
-	public void setPrev(Node prev) {
+	void setPrev(Node prev) {
 		this.prev = prev;
 	}
 
-	public Node getNext() {
+	Node getNext() {
 		return next;
 	}
 
-	public void setNext(Node next) {
+	void setNext(Node next) {
 		this.next = next;
 	}
 }
